acec6d5e396bd7d923646cc4de7bb4dafa2639fa
VincentHuber <155532950+VincentHuber@users.noreply.github.com>
2024-05-20 15:56:12 +0200

    animation des boutons, affichage des cartes

--- screens/TabScreens/FeedScreen.js
+++ screens/TabScreens/FeedScreen.js
@@ -1,7 +1,17 @@
-import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
-import React, { useEffect, useState } from 'react'
-import { db } from '../../firebase';
-import { collection, get, doc, getDocs } from 'firebase/firestore';
+import {
+  SafeAreaView,
+  Text,
+  View,
+  Button,
+  PanResponder,
+  Dimensions,
+  Animated,
+} from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { db } from "../../firebase";
+import { collection, getDocs } from "firebase/firestore";
+import Candidate from "../../components/Candidate";
+import Footer from "../../components/Footer";
 
 import {
   useFonts,
@@ -15,14 +25,55 @@ import {
   Lexend_200ExtraLight,
   Lexend_100Thin,
 } from "@expo-google-fonts/lexend";
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+////////////////////
+const { width, height } = Dimensions.get("screen");
+////////////////////
 
 const FeedScreen = () => {
-
   //Valeur de l'id
   const [id, setId] = useState(null);
 
+  const [userFiltered, setUserFiltered] = useState([]);
+
+  ////////////////////
+  const swipe = useRef(new Animated.ValueXY()).current;
+  const titlSign = useRef(new Animated.Value(1)).current;
+
+  const panResponder = PanResponder.create({
+    onMoveShouldSetPanResponder: () => true,
+
+    onPanResponderMove: (_, { dx, dy, y0 }) => {
+      swipe.setValue({ x: dx, y: dy });
+      titlSign.setValue(y0 > (height * 0.9) / 2 ? 1 : -1);
+    },
+    onPanResponderRelease: (_, { dx, dy }) => {
+      const direction = Math.sign(dx);
+      const isActionActive = Math.abs(dx) > 100;
+
+      if (isActionActive) {
+        Animated.timing(swipe, {
+          duration: 100,
+          toValue: {
+            x: direction * 500,
+            y: dy,
+          },
+          useNativeDriver: true,
+        }).start();
+      } else {
+        Animated.spring(swipe, {
+          toValue: {
+            x: 0,
+            y: 0,
+          },
+          useNativeDriver: true,
+          friction: 5,
+        }).start();
+      }
+    },
+  });
+  ////////////////////
 
   //Récupère l'id dans l'AsyncStorage
   useEffect(() => {
@@ -43,55 +94,77 @@ const FeedScreen = () => {
     fetchToken();
   }, []);
 
-
   //Récupère toutes les données des users
   useEffect(() => {
     const fetchUsers = async () => {
       try {
         const allUsers = await getDocs(collection(db, "users"));
-        
-        allUsers.forEach((doc) => {
-          console.log("Users : ", doc.id, " => ", doc.data());
+        const usersData = [];
+
+        allUsers.forEach((user) => {
+          const userObj = {
+            username: user.data().username,
+            gender: user.data().gender,
+            audioProfile: user.data().audioProfile,
+          };
+          usersData.push(userObj);
         });
+        setUserFiltered(usersData);
       } catch (error) {
         console.log("error fetching users : ", error);
       }
-    }
-  
+    };
+
     fetchUsers();
   }, []);
+  ///////////
 
+  //Chargement de la police
+  const [fontsLoaded] = useFonts({
+    Lexend_900Black,
+    Lexend_800ExtraBold,
+    Lexend_700Bold,
+    Lexend_600SemiBold,
+    Lexend_500Medium,
+    Lexend_400Regular,
+    Lexend_300Light,
+    Lexend_200ExtraLight,
+    Lexend_100Thin,
+  });
 
-
-
-//Chargement de la police
-const [fontsLoaded] = useFonts({
-  Lexend_900Black,
-  Lexend_800ExtraBold,
-  Lexend_700Bold,
-  Lexend_600SemiBold,
-  Lexend_500Medium,
-  Lexend_400Regular,
-  Lexend_300Light,
-  Lexend_200ExtraLight,
-  Lexend_100Thin,
-});
-
-
-//Attente de le chargement de la police
-if (!fontsLoaded) {
-  return null;
-}
+  //Attente de le chargement de la police
+  if (!fontsLoaded) {
+    return null;
+  }
 
   return (
-    
-    <SafeAreaView style={{flex:1, backgroundColor:"black"}}>
-      <Text style={{color:"white"}}>FeedScreen</Text>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "black",
+        alignItems: "center",
+      }}
+    >
+      {userFiltered
+        .map(({ username, audioProfile }, index) => {
+          const isFirst = index == 0;
+          const dragHandlers = isFirst ? panResponder.panHandlers : {};
+          return (
+            <Candidate
+              key={index}
+              username={username}
+              audioProfile={audioProfile}
+              isFirst={isFirst}
+              swipe={swipe}
+              titlSign={titlSign}
+              {...dragHandlers}
+            />
+          );
+        })
+        .reverse()}
+      <Footer />
     </SafeAreaView>
- 
-  )
-}
+  );
+};
 
-export default FeedScreen
-
-const styles = StyleSheet.create({})
+export default FeedScreen;
